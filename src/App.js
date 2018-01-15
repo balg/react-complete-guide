5062e39f2540e6f4ecdb9bf71776c1ace034e7c5
balg <balazs.jilling@gmail.com>
2018-01-15 17:59:03 +0100

    Lecture 39: App component got a method!

--- src/App.js
+++ src/App.js
@@ -11,12 +11,19 @@ class App extends Component {
     ]
   }
 
+  //konvencio: camelcase + Olyan method, amit nem fogsz explicit hivni, hanem event handlerkent hasznalod (pl buttonra akasztod)
+  // annak a neve Handler-re vegzodjon
+  switchNameHandler = () => {
+     console.log('Klikk!');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is just a test.</p>
-        <button>Switch Name</button>
+        {/* onClick-nek referencia alapjan kell atadni a method-ot. Ha moge tenned a ()-t (this.switchNameHandler()), akkor renderkor lefutna egyszer es ennyi. */}
+        <button onClick={ this.switchNameHandler }>Switch Name</button>
         <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age } />
         <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age }>My Hobbies: Racing</Person>
         <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age } />
