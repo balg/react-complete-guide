d0c06565e14d2fd17df36fd86cc11cff0f47736f
balg <balazs.jilling@gmail.com>
2018-02-07 17:36:27 +0100

    Lecture 50: conditional rendering without ternary op.

--- src/App.js
+++ src/App.js
@@ -52,6 +52,30 @@ class App extends Component {
       cursor: "pointer",
     };
 
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+          <Person 
+            name={ this.state.persons[0].name }
+            age={ this.state.persons[0].age } />
+          <Person 
+            name={ this.state.persons[1].name } 
+            age={ this.state.persons[1].age }
+            // .bind-dal is at lehet adni az argumentumot. Ez standard javascript megoldas, amivel a this problematikat is meg lehet oldani.
+            // Ez a preferalt megoldas
+            click={ this.switchNameHandler.bind(this, "Max!") }
+            changed={ this.nameChangedHandler }>
+              My Hobbies: Racing
+          </Person>
+          <Person 
+            name={ this.state.persons[2].name } 
+            age={ this.state.persons[2].age } />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
@@ -60,27 +84,8 @@ class App extends Component {
         <button 
           style={ style }
           onClick={ this.togglePersonsHandler }>Toggle Persons</button>
-        {
-          this.state.showPersons ? 
-            <div>
-              <Person 
-                name={ this.state.persons[0].name }
-                age={ this.state.persons[0].age } />
-              <Person 
-                name={ this.state.persons[1].name } 
-                age={ this.state.persons[1].age }
-                // .bind-dal is at lehet adni az argumentumot. Ez standard javascript megoldas, amivel a this problematikat is meg lehet oldani.
-                // Ez a preferalt megoldas
-                click={ this.switchNameHandler.bind(this, "Max!") }
-                changed={ this.nameChangedHandler }>
-                  My Hobbies: Racing
-              </Person>
-              <Person 
-                name={ this.state.persons[2].name } 
-                age={ this.state.persons[2].age } />
-            </div>
-          : null
-        }
+
+        {persons}
       </div>
     );
   }
