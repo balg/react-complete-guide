e89ddc482772e590c5c0197d6612ef9395a88aab
balg <2700278+balg@users.noreply.github.com>
2020-01-24 17:20:23 +0100

    Pass props to the wrapped component in hoc

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
+import withClass from '../../../hoc/withClass';
 import styles from './Person.css';
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...')
     return (
-      // <div className={styles.Person}>
       <React.Fragment>
         <p className={`paragraph ${styles.intro}`} onClick={this.props.click} > I'm {this.props.name} and I am {this.props.age} years old!</p>
         <p> {this.props.children}</p >
         <input type="text" onChange={this.props.changed} value={this.props.name} />
       </React.Fragment>
-      // </div>
     )
   }
 }
 
-export default Person;
+export default withClass(Person, styles.Person);
