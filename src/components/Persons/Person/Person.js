7decc26e715d3eacd44d6ba5fd53bcbf2157055a
balg <2700278+balg@users.noreply.github.com>
2020-01-24 17:44:11 +0100

    Use refs in class based components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,13 +5,30 @@ import withClass from '../../../hoc/withClass';
 import styles from './Person.css';
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputReference = React.createRef();
+  }
+
+  componentDidMount() {
+    console.log('[Person.js] focus', this.inputReference.current)
+    // this.inputReference.focus();
+    this.inputReference.current.focus();
+  }
+
   render() {
     console.log('[Person.js] rendering...')
     return (
       <React.Fragment>
         <p className={`paragraph ${styles.intro}`} onClick={this.props.click} > I'm {this.props.name} and I am {this.props.age} years old!</p>
         <p> {this.props.children}</p >
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
+        <input
+          // ref={(inputRef) => {this.inputReference = inputRef}}
+          ref={this.inputReference}
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
       </React.Fragment>
     )
   }
