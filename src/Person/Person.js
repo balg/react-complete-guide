c37c7a662299a05edec9fb12150be3e596c4b85b
balg <2700278+balg@users.noreply.github.com>
2020-01-24 13:35:58 +0100

    Mixing global styles with css modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,7 @@ import styles from './Person.css';
 const Person = (props) => {
   return (
     <div className={styles.Person}>
-      <p onClick={props.click} > I'm {props.name} and I am {props.age} years old!</p>
+      <p className={`paragraph ${styles.intro}`} onClick={props.click} > I'm {props.name} and I am {props.age} years old!</p>
       <p> {props.children}</p >
       <input type="text" onChange={props.changed} value={props.name} />
     </div>
