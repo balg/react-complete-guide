70705477c7ec99ecb7733b868239620b51011f9b
balg <2700278+balg@users.noreply.github.com>
2020-01-27 17:23:27 +0100

    Basic navigation with mock links

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import styles from './Toolbar.module.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const Toolbar = props => (
   <header className={styles.toolbar}>
     <div>MENU</div>
-    <div>LOGO</div>
+    <Logo />
     <nav>
-      ...
+      <NavigationItems />
     </nav>
   </header>
 );
