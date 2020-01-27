3baabf4ccd18b3f95f9bb3907b2ff89b40835443
balg <2700278+balg@users.noreply.github.com>
2020-01-27 13:49:42 +0100

    Backdrop added behind modal

---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './Backdrop.module.css';
+
+const Backdrop = props => props.show
+  ? <div
+    className={styles.backdrop}
+    onClick={props.clicked}
+  />
+  : null;
+
+export default Backdrop;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
 import styles from './Modal.module.css';
+import Backdrop from '../Backdrop/Backdrop';
 
 const Modal = props => (
-  <div
-    className={styles.modal}
-    style={{
-      transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-      opacity: props.show ? '1' : '0'
-    }}
-  >
-    { props.children }
-  </div>
+  <>
+    <Backdrop
+      show={props.show}
+      clicked={props.modalClosed}
+    />
+    <div
+      className={styles.modal}
+      style={{
+        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.show ? '1' : '0'
+      }}
+    >
+      {props.children}
+    </div>
+  </>
 );
 
 export default Modal;
