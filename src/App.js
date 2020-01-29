40a5aee2dc759ca8a53d8a8ec905f094dd3e63f5
balg <2700278+balg@users.noreply.github.com>
2020-01-29 13:34:23 +0100

    Applying starting setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React App!</h1>
+        <Blog />
       </div>
     );
   }
