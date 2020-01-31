e645cf3e3b38a2b6aa8b983e139582cc8c71c52d
balg <2700278+balg@users.noreply.github.com>
2020-01-31 13:01:49 +0100

    Lazy loading NewPost component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,10 +3,19 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
 import Navigation from '../../components/Navigation/Navigation';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
+    state = {
+        auth: true
+    };
+
     render() {
         return (
             <div>
@@ -14,7 +23,7 @@ class Blog extends Component {
                     <Navigation />
                 </header>
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts" />
                 </Switch>
