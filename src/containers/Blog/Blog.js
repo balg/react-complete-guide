f4348e0db6a7f11b83ed8bef2e317f51d83a99ea
balg <2700278+balg@users.noreply.github.com>
2020-01-31 14:19:52 +0100

    Lazy loading with React.Suspense

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
@@ -7,9 +7,11 @@ import Navigation from '../../components/Navigation/Navigation';
 import asyncComponent from '../../hoc/asyncComponent';
 // import NewPost from './NewPost/NewPost';
 
-const AsyncNewPost = asyncComponent(() => {
-    return import('./NewPost/NewPost');
-});
+// const AsyncNewPost = asyncComponent(() => {
+//     return import('./NewPost/NewPost');
+// });
+
+const NewPost = React.lazy(() => import('./NewPost/NewPost'));
 
 class Blog extends Component {
     state = {
@@ -23,7 +25,14 @@ class Blog extends Component {
                     <Navigation />
                 </header>
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
+                    {/* {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null} */}
+                    {this.state.auth
+                        ? <Route path="/new-post" render={() => (
+                            <Suspense fallback={<div>Loading...</div>}>
+                                <NewPost />
+                            </Suspense>
+                        )} />
+                        : null}
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts" />
                 </Switch>
