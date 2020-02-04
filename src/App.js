1487b4c05de770cbd2571a27936b925513b15b92
balg <2700278+balg@users.noreply.github.com>
2020-02-04 17:37:00 +0100

    Apply starter project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+
+import Counter from './containers/Counter/Counter';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React App!</h1>
+       <Counter />
       </div>
     );
   }
