f0074b89252fb9372fcde7997a603a52fe2398e8
balg <2700278+balg@users.noreply.github.com>
2020-02-07 11:55:31 +0100

    Store and delete results in redux store

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -5,22 +5,37 @@ import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    render () {
+    render() {
         console.log('[Render]', this.props);
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 5" clicked={() => this.props.onAddCounter(5)}  />
-                <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractCounter(5)}  />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+                <CounterControl label="Add 5" clicked={() => this.props.onAddCounter(5)} />
+                <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractCounter(5)} />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {
+                        this.props.results.map(result => (
+                            <li
+                                key={result.id}
+                                onClick={() => {this.props.onDeleteResult(result.id)}}
+                            >
+                                {result.value}
+                            </li>
+                        ))
+                    }
+                </ul>
             </div>
         );
     }
 }
 
 const mapStateToProps = state => ({
-    ctr: state.counter
+    ctr: state.counter,
+    results: state.results,
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -37,6 +52,13 @@ const mapDispatchToProps = dispatch => ({
     onSubtractCounter: (value) => dispatch({
         type: 'SUBTRACT',
         value,
+    }),
+    onStoreResult: () => dispatch({
+        type: 'STORE_RESULT',
+    }),
+    onDeleteResult: (resultId) => dispatch({
+        type: 'DELETE_RESULT',
+        resultId,
     }),
 });
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,11 @@
 const initialState = {
   counter: 0,
+  results: [],
 }
 
 const reducer = (state = initialState, action) => {
   console.log('[Reducer]', action);
-  const { type, value } = action;
+  const { type, value, resultId } = action;
   
   switch (type) {
     case 'INCREMENT':
@@ -26,6 +27,26 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         counter: state.counter - value,
+      }
+    case 'STORE_RESULT':
+      return {
+        ...state,
+        results: [...state.results, {
+          id: new Date(),
+          value: state.counter,
+        }],
+      }
+    case 'DELETE_RESULT':
+      // const updatedResults = [...state.results];
+      // updatedResults.splice(
+      //   state.results.findIndex(({ id }) => id === resultId),
+      //   1
+      // );
+      // Using Filter method is shorter
+      const updatedResults = state.results.filter(({ id }) => id !== resultId);
+      return {
+        ...state,
+        results: updatedResults
       }
     default:
       break;
