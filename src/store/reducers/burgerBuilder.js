23747504618c9568c5eaaf31c1cfb3a806681879
balg <2700278+balg@users.noreply.github.com>
2020-03-24 20:00:28 +0100

    Some code tidy

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -13,6 +13,30 @@ const initialState = {
   error: false
 };
 
+const addIngredient = (state, action) => {
+  const { ingrdntName } = action;
+  return {
+    ...state,
+    ingredients: {
+      ...state.ingredients,
+      [ingrdntName]: state.ingredients[ingrdntName] + 1
+    },
+    totalPrice: state.totalPrice + BurgerIngredientData[ingrdntName].price
+  };
+};
+
+const removeIngredient = (state, action) => {
+  const { ingrdntName } = action;
+  return {
+    ...state,
+    ingredients: {
+      ...state.ingredients,
+      [ingrdntName]: state.ingredients[ingrdntName] - 1
+    },
+    totalPrice: state.totalPrice - BurgerIngredientData[ingrdntName].price
+  };
+};
+
 const setIngredients = (state, action) => {
   const { ingredients } = action;
   return {
@@ -29,26 +53,12 @@ const fetchIngredientsFailed = state => ({
 });
 
 const reducer = (state = initialState, action) => {
-  const { type, ingrdntName } = action;
+  const { type } = action;
   switch (type) {
     case actionTypes.ADD_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [ingrdntName]: state.ingredients[ingrdntName] + 1
-        },
-        totalPrice: state.totalPrice + BurgerIngredientData[ingrdntName].price
-      };
+      return addIngredient(state, action);
     case actionTypes.REMOVE_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [ingrdntName]: state.ingredients[ingrdntName] - 1
-        },
-        totalPrice: state.totalPrice - BurgerIngredientData[ingrdntName].price
-      };
+      return removeIngredient(state, action);
     case actionTypes.SET_INGREDIENTS:
       return setIngredients(state, action);
     case actionTypes.FETCH_INGREDIENTS_FAILED:
