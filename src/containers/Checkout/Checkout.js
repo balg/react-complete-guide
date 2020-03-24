e31c816d1b55b6f6080bc7af854c3ec346cc3301
balg <2700278+balg@users.noreply.github.com>
2020-03-24 18:46:06 +0100

    Small fix for Checkout page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,44 +1,42 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 
-import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-import ContactData from './ContactData/ContactData';
+import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import ContactData from "./ContactData/ContactData";
 
 class Checkout extends Component {
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
-  }
+  };
 
   checkoutContinuedHandler = () => {
-    this.props.history.replace('checkout/contact-data');
-  }
+    this.props.history.replace("checkout/contact-data");
+  };
 
   render() {
-    return (
-      <div>
-        {
-          this.props.ingrdnts
-            ? (
-              <CheckoutSummary
-                ingredients={this.props.ingrdnts}
-                checkoutCancelled={this.checkoutCancelledHandler}
-                checkoutContinued={this.checkoutContinuedHandler}
-              />
-            )
-            : null
-        }
-        <Route
-          path={`${this.props.match.path}/contact-data`}
-          component={ContactData}
-        />
-      </div>
-    );
+    let summary = <Redirect to="/" />;
+    if (this.props.ingrdnts) {
+      summary = (
+        <>
+          <CheckoutSummary
+            ingredients={this.props.ingrdnts}
+            checkoutCancelled={this.checkoutCancelledHandler}
+            checkoutContinued={this.checkoutContinuedHandler}
+          />
+          <Route
+            path={`${this.props.match.path}/contact-data`}
+            component={ContactData}
+          />
+        </>
+      );
+    }
+    return summary;
   }
 }
 
 const mapStateToProps = state => ({
-  ingrdnts: state.ingredients,
+  ingrdnts: state.ingredients
 });
 
 export default connect(mapStateToProps)(Checkout);
