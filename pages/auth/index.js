0c49450a0222027d51dddc7c9f61058a86421606
balg <2700278+balg@users.noreply.github.com>
2020-04-17 18:12:52 +0200

    getInitialProps

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 import User from '../../components/User';
 
-const AuthIndexPage = () => (
+const AuthIndexPage = (props) => (
   <div>
-    <h1>The Auth Page</h1>
+    <h1>The Auth Page {props.appName}</h1>
     <User name="Balika" age="33" />
   </div>
 );
+
+AuthIndexPage.getInitialProps = context => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ appName: 'Super App' })
+    }, 1000);
+  });
+  return promise;
+}
 
 export default AuthIndexPage;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import Link from "next/link";
 import Router from "next/router";
 
-const IndexPage = () => (
-  <div>
-    <h1>The Main Page</h1>
-    <p>
-      Go to{" "}
-      <Link href="/auth">
-        <a>Auth</a>
-      </Link>
-    </p>
-    <button onClick={() => Router.push('/auth')}>Go to Auth</button>
-  </div>
-);
+class IndexPage extends Component {
+  static getInitialProps(context) {
+    console.log('req: ' + context.req)
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ appName: 'Super App' })
+      }, 1000);
+    });
+    return promise;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.appName}</h1>
+        <p>
+          Go to{" "}
+          <Link href="/auth">
+            <a>Auth</a>
+          </Link>
+        </p>
+        <button onClick={() => Router.push("/auth")}>Go to Auth</button>
+      </div>
+    );
+  }
+}
 
 export default IndexPage;
