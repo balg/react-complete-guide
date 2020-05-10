19236616cf8a820a9e8b150692de8624f3c5df52
balg <2700278+balg@users.noreply.github.com>
2020-05-10 17:52:42 +0200

    useReducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useReducer, useEffect, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -6,72 +6,107 @@ import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
 import { fireBasePath, headers } from "../../utils/fetch";
 
+const ingredientReducer = (currentIngredients, action) => {
+  const { type, ingredients, ingredient, ingredientId } = action;
+  switch (type) {
+    case "SET":
+      return ingredients;
+    case "ADD":
+      return [...currentIngredients, ingredient];
+    case "DELETE":
+      return currentIngredients.filter((i) => i.id !== ingredientId);
+    default:
+      throw new Error("Should not get there!");
+  }
+};
+
+const httpReducer = (httpState, action) => {
+  const { type, errorData } = action;
+  switch (type) {
+    case "SEND":
+      return { loading: true, error: null };
+    case "RESPONSE":
+      return { ...httpState, loading: false };
+    case "ERROR":
+      return { loading: false, error: errorData };
+    case "CLEAR":
+      return { ...httpState, error: null };
+    default:
+      throw new Error("Should not get there!");
+  }
+};
+
 const Ingredients = () => {
-  const [ingredients, setIngredients] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const [ingredients, dispatch] = useReducer(ingredientReducer, []);
+  const [httpState, dispatchHttp] = useReducer(httpReducer, {
+    loading: false,
+    error: null,
+  });
 
   useEffect(() => {
     console.log("RENDERING INGREDIENTS");
   });
 
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
-    setIngredients(filteredIngredients);
+    dispatch({
+      type: "SET",
+      ingredients: filteredIngredients,
+    });
   }, []);
 
   const addIngredientHandler = (ingredient) => {
-    setIsLoading(true);
+    dispatchHttp({ type: "SEND" });
     fetch(`${fireBasePath}/ingredients.json`, {
       method: "POST",
       body: JSON.stringify(ingredient),
       headers,
     })
       .then((response) => {
-        setIsLoading(false);
+        dispatchHttp({ type: "RESPONSE" });
         return response.json();
       })
       .then((responseData) => {
-        setIngredients((prev) => [
-          ...prev,
-          {
+        dispatch({
+          type: "ADD",
+          ingredient: {
             id: responseData.name,
             ...ingredient,
           },
-        ]);
+        });
       })
       .catch((err) => {
-        setError("Something went wrong!");
-        setIsLoading(false);
+        dispatchHttp({ type: "ERROR", errorData: err });
       });
   };
 
-  const removeIngredientHandler = (id) => {
-    setIsLoading(true);
-    fetch(`${fireBasePath}/ingredients/${id}.json`, {
+  const removeIngredientHandler = (ingredientId) => {
+    dispatchHttp({ type: "SEND" });
+    fetch(`${fireBasePath}/ingredients/${ingredientId}.json`, {
       method: "DELETE",
     })
       .then((response) => {
-        setIsLoading(false);
-        setIngredients((prev) =>
-          prev.filter((ingredient) => ingredient.id !== id)
-        );
+        dispatchHttp({ type: "RESPONSE" });
+        dispatch({
+          type: "DELETE",
+          ingredientId,
+        });
       })
       .catch((err) => {
-        setError("Something went wrong!");
-        setIsLoading(false);
+        dispatchHttp({ type: "ERROR", errorData: err });
       });
   };
 
   const clearError = () => {
-    setError(null);
+    dispatch({ type: "CLEAR" });
   };
 
+  const { loading, error } = httpState;
   return (
     <div className="App">
       {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
       <IngredientForm
         onAddIngredient={addIngredientHandler}
-        loading={isLoading}
+        loading={loading}
       />
 
       <section>
