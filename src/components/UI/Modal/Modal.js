00eb8d0936c20cbc030993c2d31b8c4604c40765
balg <2700278+balg@users.noreply.github.com>
2020-07-03 12:08:11 +0200

    React memo compare function

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import styles from './Modal.module.css';
-import Backdrop from '../Backdrop/Backdrop';
+import styles from "./Modal.module.css";
+import Backdrop from "../Backdrop/Backdrop";
 
-const Modal = props => (
+const Modal = (props) => (
   <>
-    <Backdrop
-      show={props.show}
-      clicked={props.modalClosed}
-    />
+    <Backdrop show={props.show} clicked={props.modalClosed} />
     <div
       className={styles.modal}
       style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
+        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+        opacity: props.show ? "1" : "0",
       }}
     >
       {props.children}
     </div>
   </>
 );
-
-export default React.memo(Modal);
+// This still does not prevent re-renders as orderSummary (will be in children) will be re-rendered on every keystroke in BurgerBuilder
+export default React.memo(
+  Modal,
+  (prevProps, nextProps) =>
+    nextProps.show === prevProps.show &&
+    nextProps.children === prevProps.children
+);
